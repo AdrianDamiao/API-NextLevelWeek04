de6a4c60cc240374e27df48cb6b433e727a6df5b
Adrian Damião <adriandami_@hotmail.com>
2021-02-24 14:06:36 -0300

    Dia 3 da Next Level Week 04
    Testes de Integracão

--- src/routes.ts
+++ src/routes.ts
@@ -1,10 +1,16 @@
 import { Router } from 'express';
+import { SurveyController } from './controllers/SurveyController';
 import { UserController } from './controllers/UserController';
+import { SurveyRepository } from './repositories/SurveysRepository';
 
 const router = Router();
 
 const userController = new UserController();
+const surveysController = new SurveyController();
 
 router.post('/users', userController.create);
+
+router.post("/surveys", surveysController.create);
+router.get("/surveys", surveysController.show);
 
 export { router };
--- src/server.ts
+++ src/server.ts
@@ -1,34 +1,3 @@
-import 'reflect-metadata'
-import express, { request, response } from 'express';
-import './database';
-import { router } from './routes';
+import { app } from "./app"
 
-const app = express();
-
-app.use(express.json());
-app.use(router);
-
-/**
- * Métodos HTTP Disponíveis
- *  GET => Busca
- * POST => Salvar Informacoes
- * PUT => Alterar
- * DELETE => Deletar
- * PATCH => Alteração Específica
- 
-
-//http://localhost:3333/users
-app.get("/", (request, response) => {
-    //return response.send("Hello World - NLW04");
-    return response.json({message: "NLW04"}); //Json é o mais usual de ser retornado
-});
-
-//1° param => Rota(Recurso API) a rota / so pode ser usada se for em metodos diferentes
-//2° para => request, response
-app.post("/", (request, response) => { 
-    //Recebeu os dados para salvar
-    return response.json({message: "Os dados foram salvos com sucesso"})
-});
-
-//Transpile nao vai checar erros de sintaxe, ignore vai ignorar o que acontece em node modules*/
 app.listen(3333, () => console.log("Server is running!"));
